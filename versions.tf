fd0bed3057e6bbff7350017c4235d10bd4fae4ea
Amal Sarkar <amal@cognizer.ai>
2022-08-16 12:53:38 +0530

    added terraform code

---
+++ versions.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  cloud {
+    organization = "sarkaraml"
+
+    workspaces {
+      name = "ml-terraform"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
